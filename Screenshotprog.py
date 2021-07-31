be1359cfdc817351ff8fff786e502ebcd3b5a8c7
Loughum <80972720+Loughum@users.noreply.github.com>
2021-07-31 15:37:43 +0300

    Update Screenshotprog.py

--- Screenshotprog.py
+++ Screenshotprog.py
@@ -1,4 +1,5 @@
 import pyautogui
 print('make sure that the Pillow module is installed, if it`s not, print in cmd "pip install Pillow" and try again"'+'\n'+'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'+'\n')
+im_0 = pyautogui.screebshot()
 im_save = pyautogui.screenshot('screenshotprog.png')
 print ('now you can find image "screenshotprog" on your PC')
